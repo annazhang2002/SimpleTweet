f4cebec517eb5dbd0d31e45f0fea06f52ef6ab20
Anna Zhang <annayzhang@fb.com>
2020-06-29 15:57:52 -0700

    Added compose feature to allow users to post tweets

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -3,9 +3,12 @@ package com.codepath.apps.restclienttemplate.models;
 import android.text.format.DateUtils;
 import android.util.Log;
 
+import com.codepath.apps.restclienttemplate.TwitterApp;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -13,11 +16,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
+@Parcel
 public class Tweet {
 
     public String body;
     public  String createdAt;
     public User user;
+
+    public Tweet() {}
 
     public static Tweet fromJson(JSONObject object) throws JSONException {
         Tweet tweet = new Tweet();
